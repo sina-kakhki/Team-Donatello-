0c644358895d75b52924dd46f5368ac89a9ef920
Johann Besas <73777925+johannbesas@users.noreply.github.com>
2021-03-26 14:36:24 +1300

    messenger feature in progress

---
+++ public/scripts/scripts.js
@@ -0,0 +1,12 @@
+function processForm(e) {
+  e.preventDefault()
+  console.log('hi')
+  return false
+}
+
+// const form = document.getElementById("chatForm")
+// if (form.attachEvent) {
+//   form.attachEvent("submit", processForm)
+// } else {
+//   form.addEventListener("submit", processForm)
+// }
